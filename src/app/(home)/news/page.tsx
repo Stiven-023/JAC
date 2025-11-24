d103cb8518829eaf24c7eba4ab5ab1bb63320dce
Vtinasr <valentina.sanchez.rosero@correounivalle.edu.co>
2025-11-24 09:22:42 -0500

    Ajustes esteticos de la seccion noticias, eventos y servicios

--- src/app/(home)/news/page.tsx
+++ src/app/(home)/news/page.tsx
@@ -3,7 +3,7 @@ import Card from "@/components/Card";
 export default function Page() {
     return (
         <div className="flex flex-col justify-start">
-            <h1 className="text-center mb-4">Noticias</h1>
+            <h1 className="-mt-10 mb-6 text-center text-3xl font-black tracking-tight text-[#0f0f0f]">Noticias</h1>
             <div className="grid grid-cols-1 gap-4 px-6 text-sm 
                       md:grid-cols-2 md:px-10 md:text-base 
                       lg:grid-cols-2 lg:px-32 lg:text-sm">
